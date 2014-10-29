c984c798f0f055b1d7b360ec8ebb16f6eea613bf
Stellaris-code <Stellaris-code@users.noreply.github.com>
2014-10-29 11:34:58 +0100

    Suppression du '== 0' inutile
    Si il n'y a pas d'opérateurs de comparaison dans un if, un '== 0' est implicitement ajouté (Reliquat du C).

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		Vous pourrez envoyer des crafts, les télécharger, avec vos fonds de KSP glanés en mode carrière ou en vendant des crafts contre des fonds.</p>
 		<p><em>Dès maintenant</em>, inscrivez-vous sur KSP Craft Exchanger et ayez à votre dispositions tous les fichiers craft de notre communauté.</p>
 		<?php
-		if(isset($_SESSION['Utilisateur']) == 0)
+		if(isset($_SESSION['Utilisateur']))
 		{ echo('
 		<a href="inscription.php"><button class="btn btn-primary">Inscrivez-vous</button></a> 
 		<a href="connection.php"><button class="btn btn-danger">Connectez-vous</button></a>
