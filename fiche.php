13bde7ad8e3c90232fc5de53ff8d70c058fa8adf
dorix <dorian.bourgeoisat@me.com>
2014-12-01 21:43:59 +0100

    Ajout du caroussel d'accueil

--- fiche.php
+++ fiche.php
@@ -3,6 +3,16 @@ include('fixe.php');
 echo('<section class="col-xs-10">');
 if(isset($_GET['ID']))
 {
+	if($_SESSION['Utilisateur']) != 0)
+	{
+		$type = 'a';
+		$desactive = ''
+	}
+	else
+	{
+		$type = 'span';
+		$desactive = ' disabled="disabled"';
+	}
 	try
 	{
 		$bdd = new PDO('mysql:host=sql2.olympe.in;dbname=cxo2zffc', 'cxo2zffc', '4msupcqal4cadtjsc', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
@@ -20,7 +30,7 @@ if(isset($_GET['ID']))
 		echo('<p><h1>'.$data['Nom'].'</h1><div class="table-responsive"><table class="table table-bordered fiche"><tbody>');
 		echo('<tr><td class="col-sm-8"><img src="publications/'.$data['Nom'].'.'.$data['ImgExt'].'" class="img-responsive"></td><td class="col-sm-3"><strong>Description : </strong>'.$data['Descr'].'</td></tr>');
 		echo('<tr><td><strong>Auteur : </strong>'.$data['Auteur'].'</td><td><strong>Prix : </strong>'.$data['Prix'].'</td></tr>');
-		echo('<tr><td><strong>Type : </strong>'.$data['MODV'].', '.$data['SUB'].'</td><td><a class="btn btn-primary" href="dlpage.php?ID='.$data['ID'].'">Télécharger</a></td></tr>');
+		echo('<tr><td><strong>Type : </strong>'.$data['MODV'].', '.$data['SUB'].'</td><td><'.$type.' class="btn btn-primary"'.$desactive.' href="dlpage.php?ID='.$data['ID'].'">Télécharger</'.$type.'></td></tr>');
 		echo('</tbody></table></div></p>');
 	}
 	$req->closeCursor();
--- index.php
+++ index.php
@@ -12,6 +12,49 @@
 		<a href="inscription.php"><button class="btn btn-primary">Inscrivez-vous</button></a> 
 		<a href="connection.php"><button class="btn btn-danger">Connectez-vous</button></a>
 		');}
+		try
+		{
+			$bdd = new PDO('mysql:host=sql2.olympe.in;dbname=cxo2zffc', 'cxo2zffc', '4msupcqal4cadtjsc', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+		}
+		catch (Exception $e)
+		{
+			die('Erreur: '. $e->getMessage());
+		}
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$reponse = $bdd->query('SELECT * FROM publications ORDER BY ID DESC LIMIT 0,5');
+		echo('<div id="carousel-example-generic" class="CAROUSEL slide" data-ride="CAROUSEL">
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">');
+		$i = 0;
+		while ($data = $reponse->fetch())
+		{
+			if($i == 0)
+			{
+				$first = ' active';
+			}
+			else
+			{
+				$first = '';
+			}
+			echo('<div class="item'.$first.'">
+					<img src="publications/'.$data['Nom'].'.'.$data['ImgExt'].'" alt="'.$data['Nom'].'">
+					<div class="carousel-caption">
+						<h3>'.$data['Nom'].'</h3>
+						<a class="btn btn-primary" href="dl.php?ID='.$data['ID'].'">Fiche</a>
+					</div>
+				</div>');
+		}
+		echo('</div>
+		<!-- Controls -->
+		<a class="left CAROUSEL-control" href="#CAROUSEL-example-generic" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		</div>');
 		?>
 		</section>
 <?php include('fin.php'); ?>
