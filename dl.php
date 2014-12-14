cb139eeba84074ddb6aa0696e17a44623e470c39
dorix <dorian.bourgeoisat@me.com>
2014-12-14 10:45:28 +0100

    Ajout de google analytics

--- dl.php
+++ dl.php
@@ -1,5 +1,15 @@
 <?php include('fixe.php');
 echo('<section class="col-xs-10">');
+echo("<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57642885-1', 'auto');
+  ga('send', 'pageview');
+
+</script>");
 if(isset($_SESSION['Utilisateur']))
 {
 	try
