437a98c90f2ca3d234643b80a8f6ea7f8dae426f
dorix <dorian.bourgeoisat@me.com>
2014-12-17 22:06:55 +0100

    Modification pour ajout des votes

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php include('fixe.php'); ?>
+		<section class="col-xs-10">
+<div class="progress">
+  <div class="progress-bar progress-bar-success" style="width: 50%">
+    <span class="sr-only"></span>
+  </div>
+  <div class="progress-bar progress-bar-primary" style="width: 50%">
+    <span class="sr-only"></span>
+  </div>
+</div>
+		</section>
+<?php include('fin.php'); ?>
