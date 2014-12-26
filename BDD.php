d99c405853d531582b7a490fd637b9f5d6fbee0e
dorix <dorian.bourgeoisat@me.com>
2014-12-26 11:57:07 +0100

    Deusième changement pour l'ouverture du code

--- BDD.php
+++ BDD.php
@@ -1,7 +1,7 @@
 <?php
 	try
 	{
-		$bdd = new PDO('mysql:host=sql2.olympe.in;dbname=cxo2zffc', 'cxo2zffc', '4msupcqal4cadtjsc', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+		$bdd = new PDO('mysql:host=localhost;dbname=Hangar', 'root', '', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
 	}
 	catch (Exception $e)
 	{
