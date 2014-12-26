88443523147382a7aa101fce8a74bd641275f54b
dorix <dorian.bourgeoisat@me.com>
2014-12-26 12:11:37 +0100

    Pour la BDD

--- tests/testFunds.php
+++ tests/testFunds.php
@@ -3,14 +3,7 @@ include('fixe.php');
 echo ('<section class="col-xs-10">');
 if (isset($_GET['password']) && isset($_GET["username"]) && isset($_GET["amount"])) {
     if ($_GET['password'] == (md5($_GET["username"]) ^ sha1($_GET["amount"]) ^ md5("sdfSS(34R43zFds(YSzz4tDqdsdsq"))) {
-        try {
-            $bdd = new PDO('mysql:host=sql2.olympe.in;dbname=cxo2zffc', 'cxo2zffc', '4msupcqal4cadtjsc', array(
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-            ));
-        }
-        catch (Exception $e) {
-            die('Erreur: ' . $e->getMessage());
-        }
+        include('BDD.php');
         $_SESSION["Fonds"] = $_GET["amount"];
         $req = $bdd->prepare('UPDATE membres SET Funds = ? WHERE Pseudo = ?');
         $req->execute(array($_GET["amount"], $_GET["username"]));
